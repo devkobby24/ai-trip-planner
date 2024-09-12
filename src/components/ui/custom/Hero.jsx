1ea21361963303b2450fb7975ad1e5cc6af14116
devkobby24 <nanakobby2002@gmail.com>
2024-09-12 16:03:30 +0000

    Hero update

--- src/components/ui/custom/Hero.jsx
+++ src/components/ui/custom/Hero.jsx
@@ -5,12 +5,15 @@ import { Link } from "react-router-dom";
 function Hero() {
   return (
     <div className="flex flex-col items-center mx-56 gap-9">
-      <h1 className="font-extrabold text-[50px] text-center mt-16">
-        <span className="text-[#3dce86]">
+      <h1 className="text-center font-extrabold mt-16 px-4 sm:px-6 md:px-8 lg:px-12 text-[32px] sm:text-[36px] md:text-[40px] lg:text-[50px]">
+        <span className="text-[#3dce86] block text-[28px] sm:text-[32px] md:text-[36px] lg:text-[40px]">
           Discover Your Next Adventure with AI:
         </span>
-        Personalize Itineraries at Your Fingertips
+        <span className="block text-[24px] sm:text-[28px] md:text-[32px] lg:text-[36px]">
+          Personalize Itineraries at Your Fingertips
+        </span>
       </h1>
+
       <p className="text-xl text-gray-500 text-center">
         Your personal trip planner and travel curator, creating the custom
         itineraries tailored to your interests and budget.
@@ -25,7 +28,7 @@ function Hero() {
         className="w-full h-full mt-20 mb-40"
       />
     </div>
-  )
+  );
 }
 
 export default Hero;
