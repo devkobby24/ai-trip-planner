d80528387147c3446be118dd09885b53b5142397
devkobby24 <nanakobby2002@gmail.com>
2024-09-13 14:36:08 +0000

    PlacesToVisit update

--- src/view-trip/components/PlacesToVisit.jsx
+++ src/view-trip/components/PlacesToVisit.jsx
@@ -4,7 +4,7 @@ import PlaceCardItem from "./PlaceCardItem";
 function PlacesToVisit({ trip }) {
   return (
     <div>
-      <h2 className="font-bold text-lg">Places To Visit</h2>
+      <h2 className="font-bold text-xl">Places To Visit</h2>
       {/* <div>
         {trip?.tripData?.itinerary.map((item, index) => (
                 <div className='grid grid-cols-2'>
@@ -20,29 +20,31 @@ function PlacesToVisit({ trip }) {
 
       </div> */}
       <div>
-  {/* Check if itinerary exists and loop through the days */}
-  {trip?.tripData?.itinerary &&
-    Object.keys(trip.tripData.itinerary).map((dayKey, index) => (
-      <div key={index} className='mt-5'>
-        {/* Display the day name */}
-        <h2 className='font-medium text-lg'>{`Day ${index + 1}`}</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {/* Loop through morning, afternoon, and evening */}
-        {["morning", "afternoon", "evening"].map((timeOfDay) => {
-          const place = trip.tripData.itinerary[dayKey][timeOfDay];
-          return place && (
-            <div key={timeOfDay} className='my-3'>
-              <h2 className='font-medium text-sm text-orange-500'>🕟 {place.time}</h2>
-              <PlaceCardItem place={place} />
+        {/* Check if itinerary exists and loop through the days */}
+        {trip?.tripData?.itinerary &&
+          Object.keys(trip.tripData.itinerary).map((dayKey, index) => (
+            <div key={index} className="mt-5">
+              {/* Display the day name */}
+              <h2 className="font-medium text-lg">{`Day ${index + 1}`}</h2>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                {/* Loop through morning, afternoon, and evening */}
+                {["morning", "afternoon", "evening"].map((timeOfDay) => {
+                  const place = trip.tripData.itinerary[dayKey][timeOfDay];
+                  return (
+                    place && (
+                      <div key={timeOfDay} className="my-3">
+                        <h2 className="font-medium text-sm text-orange-500">
+                          🕟 {place.time}
+                        </h2>
+                        <PlaceCardItem place={place} />
+                      </div>
+                    )
+                  );
+                })}
+              </div>
             </div>
-          );
-        })}
-        </div>
+          ))}
       </div>
-    ))
-  }
-</div>
-
     </div>
   );
 }
